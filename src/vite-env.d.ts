ce6f85ee9ac04527ad77f1ba7f79394440a6d2b7
TakaoKishimoto <takao.k.04@gmail.com>
2023-01-17 16:21:44 -0800

    First commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  // 定義した変数は下記に追加する
+}
+
+interface ImportMeta {
+  readonly env: ImportMeta;
+}
